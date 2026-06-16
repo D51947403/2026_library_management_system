9cd350aeac513a1dcac6ebf1583eb50447c8d9cc
D51947403 <singraul20codeblog@gmail.com>
2026-06-16 17:47:37 +0530

    Basic Library Management System  demo project in Java Swing, JDBC and MySQL

---
+++ src/main/java/com/mainaha/bookstore/LibraryManagementSystem.java
@@ -0,0 +1,58 @@
+package com.mainaha.bookstore;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LibraryManagementSystem extends JFrame implements ActionListener {
+
+	JLabel l1;
+
+	public LibraryManagementSystem() {
+		setTitle("Library Management System");
+
+		setSize(1100, 550);
+		setLocation(300, 300);
+		getContentPane().setLayout(null);
+		
+		JButton b1 = new JButton("Next");
+		b1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setVisible(false);
+				Login login = new Login();
+				login.setVisible(true);
+			}
+		});
+		b1.setForeground(Color.GRAY);
+		b1.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		b1.setBounds(750, 290, 156, 50);
+		getContentPane().add(b1);
+		
+		JLabel lblNewLabel = new JLabel("");
+		// Load the image resource relative to the classpath root
+		java.net.URL imgURL = getClass().getResource("/icons/first.jpg");
+
+		if (imgURL != null) {
+			// Set the icon on the label
+			lblNewLabel.setIcon(new ImageIcon(imgURL));
+		} else {
+			System.err.println("Could not find file on classpath: /images/myImage.png");
+		}
+		//lblNewLabel.setIcon(new ImageIcon("D:\\2026-intellij-java-workspace\\2026_Library_Management_System\\src\\main\\resources\\icons\\first.jpg"));
+		lblNewLabel.setBounds(0, 0, 1350, 351);
+		getContentPane().add(lblNewLabel);
+		
+	}
+
+	public void actionPerformed(ActionEvent ae) {
+		new Login().setVisible(true);
+		this.setVisible(false);
+
+	}
+
+	public static void main(String[] args) {
+		LibraryManagementSystem window = new LibraryManagementSystem();
+		window.setVisible(true);
+	}
+}
